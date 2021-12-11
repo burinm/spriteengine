fae4de22200580af064b04a026be2f3e6a95d591
burin <21989631+burinm@users.noreply.github.com>
2021-12-10 18:17:49 -0700

    fixed character generation script screencodes

--- generate_sample_bg_matrix.sh
+++ generate_sample_bg_matrix.sh
@@ -5,18 +5,18 @@ SAMPLE=sample_matrix.bg
 rm -f $SAMPLE
 
 for i in `seq 0 39`
-    do echo -n -e \\x6a >> $SAMPLE;  # * asters
+    do echo -n -e \\x2a >> $SAMPLE;  # * asters
 done
 
 for j in `seq 0 27`
 do
-    echo -n -e \\x6a >> $SAMPLE  # * asters
+    echo -n -e \\x2a >> $SAMPLE  # * asters
     for i in `seq 0 37`
-        do echo -n -e \\x60 >> $SAMPLE;  # [ ] spaces
+        do echo -n -e \\x20 >> $SAMPLE;  # [ ] spaces
     done
-    echo -n -e \\x6a >> $SAMPLE;  # * asters
+    echo -n -e \\x2a >> $SAMPLE;  # * asters
 done
 
 for i in `seq 0 39`;
-    do echo -n -e \\x6a >> $SAMPLE;  # * asters
+    do echo -n -e \\x2a >> $SAMPLE;  # * asters
 done
