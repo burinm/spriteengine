3dcb718580af574314cc794934cb27184d32bbb3
burin <21989631+burinm@users.noreply.github.com>
2021-12-10 19:34:36 -0700

    screencode generator fixes - still broken, but useable for demo

--- generate_screencode_asci.sh
+++ generate_screencode_asci.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+Z=91
+A=65
+
 #Note, lol, this is brittle
 OFFSET=-64
 for i in `echo -n "$1" | grep -o .`;
 do
     ASCII=16#`echo -n $i | xxd -p`  # 16#, hex->decimal
-    SCREENCODE=$(($ASCII + $OFFSET))
+    if [ $(($ASCII - $A)) -ge 0 ] && [ $(($Z - $ASCII)) -ge 0 ]
+    then
+        SCREENCODE=$(($ASCII + $OFFSET))
+    else
+        SCREENCODE=$ASCII
+    fi
     #echo -n "${SCREENCODE}"
     echo  -e -n `printf '\\\x%02x' $SCREENCODE`
 done;
